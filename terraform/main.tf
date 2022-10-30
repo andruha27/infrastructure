c2e314c695965ae783c82fc7f8bb41aadb1836e7
Воробьев Андрей <a.vorobyov@city-call.ru>
2022-10-30 23:04:03 +0300

    split root to modules

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,51 +1,22 @@
-terraform {
-  required_providers {
-    yandex = {
-      source  = "yandex-cloud/yandex"
-      version = "0.61.0"
-    }
-  }
+module "tf-yc-instance" {
+  source       = "./modules/tf-yc-instance"
+  zone         = var.zone
+  platform_id  = "standard-v1"
+  image_id = "fd80qm01ah03dkqb14lc"
+  core_count   = 2
+  instance_subnet_id = module.tf-yc-network.yandex_vpc_subnets[var.zone].id
+  memory_value = 4 
 }
 
-provider "yandex" {
-  token     = "t1.9euelZrKisydnpSZx46djZrOjpnKjO3rnpWalJWZlJTOmcabnpKPx5zJnpfl9PdwPQZl-e97Ewbb3fT3MGwDZfnvexMG2w.6cKb1VjvvWy0fEZpDWviW8ZYg9L9O_Bo1E1bbrpwbNrAc81lB_GmEyzwJO11MSmgX1K67LiviHREouqhozC1Ag"
-  cloud_id  = "b1g3jddf4nv5e9okle7p"
-  folder_id = "b1gt260mflvge5lvm7ge"
-  zone      = "ru-central1-a"
+module "tf-yc-network" {
+  source = "./modules/tf-yc-network"
+  network_zones =    ["ru-central1-a", "ru-central1-b", "ru-central1-c"]
 }
 
-resource "yandex_compute_instance" "vm-1" {
-  name = "chapter5-lesson2-std-009-047"
-
-  resources {
-    cores  = 2
-    memory = 4
-  }
-
-  boot_disk {
-    initialize_params {
-      image_id = "fd80qm01ah03dkqb14lc"
-    }
-  }
-
-  network_interface {
-    subnet_id = "e9bg8cjovk4tpbk8h6m7"
-    nat       = true
-  }
-
-  metadata = {
-    user-data = "${file("userdata.yaml")}"
-	#ssh-keys = "ubuntu:${file("~/.ssh/id_rsa_ed25519.pub")}"
-
-  }
-
+output "ip_address_external" {
+    value = "${module.tf-yc-instance.ip_address_external}"
 }
 
-
-output "ip_address" {
-  value = yandex_compute_instance.vm-1.network_interface.0.ip_address
-}
-
-output "external_ip_address_vm_1" {
-  value = yandex_compute_instance.vm-1.network_interface.0.nat_ip_address
+output "ip_address_local" {
+    value = "${module.tf-yc-instance.ip_address_local}"
 }
---
+++ terraform/variables.tf
@@ -0,0 +1,23 @@
+variable "token" {
+  description = "token"
+  type        = string
+  default     = ""
+}
+
+variable "cloud_id" {
+  description = "cloud id"
+  type        = string
+  default     = "b1g3jddf4nv5e9okle7p"
+}
+
+variable "folder_id" {
+  description = "folder id"
+  type        = string
+  default     = "b1gt260mflvge5lvm7ge"
+}
+
+variable "zone" {
+  description = "zone zone"
+  type        = string
+  default     = ""
+}
